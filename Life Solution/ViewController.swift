69aa123b0ee08112a56bb70041cfd5168bf12e56
Azis Jabbar Susetio <tioazis@gmail.com>
2019-05-20 12:49:07 +0700

    Initial Commit

---
+++ Life Solution/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Life Solution
+//
+//  Created by Azis Jabbar Susetio on 20/05/19.
+//  Copyright © 2019 Azis Susetio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
